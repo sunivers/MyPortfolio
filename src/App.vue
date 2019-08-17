c2c0ac27bc8c77f4bee59900f934b10385fb54de
soyoung <soyoungp02@gmail.com>
2019-08-18 00:57:36 +0900

    메인뷰에 로고 배치

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <logo></logo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/Logo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Logo
   }
 }
 </script>
 
 <style>
+@import url("./css/reset.css");
+
+html, body, #app {
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
