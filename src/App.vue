3b76706a0068d1791fe0422a12fdeafa4f4f1178
soyoung <soyoungp02@gmail.com>
2019-08-18 01:32:12 +0900

    메인 뷰에 네비게이션 리스트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <logo></logo>
+    <ul class="nav-list">
+      <li>personal project</li>
+      <li>about me</li>
+      <li>company project</li>
+      <li>contact me</li>
+    </ul>
   </div>
 </template>
 
@@ -24,7 +30,25 @@ html, body, #app {
 #app {
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.nav-list {
+  position: absolute;
+  width: 310px;
+}
+.nav-list li {
+  width: 100px;
+  height: 50px;
+  display: inline-block;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 1rem;
+  font-weight: 100;
+  text-transform: uppercase;
+}
+.nav-list li:nth-child(odd) {
+  margin-right: 110px;
 }
 </style>
