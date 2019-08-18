1e63c42df1f96c2f9638ccfbf0d71ef02223b618
soyoung <soyoungp02@gmail.com>
2019-08-18 19:01:32 +0900

    뷰에 logo, toolbar 컴포넌트 주입

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        About
+    <div class="container">
+        <router-link to="/"><logo></logo></router-link>
+        <tool-bar></tool-bar>
     </div>
 </template>
 
 <script>
-export default {
+import Logo from '../components/Logo.vue';
+import ToolBar from '../components/ToolBar.vue';
 
+export default {
+    components: {
+        Logo,
+        ToolBar,
+    }
 }
 </script>
 
