b78d83c65003606a0f50d88502175245941b21f2
soyoung <soyoungp02@gmail.com>
2019-08-18 15:03:00 +0900

    ToolBar 컴포넌트 추가

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <router-link to="/company">company p.j</router-link>
+        <router-link to="/about">about me</router-link>
+        <router-link to="/personal">personal p.j</router-link>
+        <router-link to="/contact">contact me</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+    padding: 8px;
+    text-align: center;
+    border-bottom: 1px solid #a0a0a0;
+    margin: 20px;
+}
+.header a.router-link-exact-active {
+    color: #42b883;
+    font-weight: 400;
+}
+.header a {
+    color: #333;
+    padding: 0 12px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 100;
+}
+.header a:hover {
+    text-decoration: underline;
+}
+</style>
