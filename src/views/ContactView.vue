4b1d7ecd75a1cc69d52b6bb6213e2e5866e8c197
soyoung <soyoungp02@gmail.com>
2019-08-20 00:39:56 +0900

    핸드폰, 메일, 블로그, 깃헙 연락수단 추가 완료

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,6 +2,19 @@
     <div class="container">
         <router-link to="/"><logo></logo></router-link>
         <tool-bar></tool-bar>
+        <div class="contact-icon">
+            <a href="tel:010-3013-3596"><i class="fas fa-mobile-alt"></i></a>
+        </div>
+        <div class="contact-icon">
+            <a href="mailto:soyoungp02@gmail.com"><i class="far fa-envelope"></i></a>
+        </div>
+        <div class="contact-icon">
+            <a href="https://psyvelop.blogspot.com/" target="_blank"><i class="fab fa-blogger"></i></a>
+        </div>
+        <div class="contact-icon">
+            <a href="https://github.com/sunivers" target="_blank"><i class="fab fa-github"></i></a>
+        </div>
+        <!-- <div class="contact-icon"><i class="fab fa-linkedin-in"></i></div> -->
     </div>
 </template>
 
@@ -17,6 +30,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+@import url('../../node_modules/@fortawesome/fontawesome-free/css/all.css');
+.contact-icon {
+    text-align: center;
+    padding: 10px 0;
+}
+.contact-icon i {
+    font-size: 70px;
+    color: #aaa;
+}
+.contact-icon i:hover {
+    color: #42b883;
+}
 </style>
