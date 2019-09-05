e6e599969125efc3419df8885517ce43a241164f
soyoung <soyoungp02@gmail.com>
2019-09-05 15:13:49 +0900

    블로그 링크 수정

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -11,7 +11,7 @@
                 <a href="mailto:soyoungp02@gmail.com"><i class="far fa-envelope"></i></a>
             </div>
             <div class="contact-icon">
-                <a href="https://psyvelop.blogspot.com/" target="_blank"><i class="fab fa-blogger"></i></a>
+                <a href="https://soobakba.tistory.com/" target="_blank"><i class="fab fa-blogger"></i></a>
             </div>
             <div class="contact-icon">
                 <a href="https://github.com/sunivers" target="_blank"><i class="fab fa-github"></i></a>
@@ -27,24 +27,24 @@ import Logo from '../components/Logo.vue';
 import ToolBar from '../components/ToolBar.vue';
 
 export default {
-    components: {
-        Logo,
-        ToolBar,
-    }
-}
+	components: {
+		Logo,
+		ToolBar,
+	},
+};
 </script>
 
 <style scoped>
 @import url('../../node_modules/@fortawesome/fontawesome-free/css/all.css');
 .contact-icon {
-    text-align: center;
-    padding: 20px 0;
+	text-align: center;
+	padding: 20px 0;
 }
 .contact-icon i {
-    font-size: 70px;
-    color: #aaa;
+	font-size: 70px;
+	color: #aaa;
 }
 .contact-icon i:hover {
-    color: #42b883;
+	color: #42b883;
 }
 </style>
